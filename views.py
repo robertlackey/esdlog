5fdef0e7a049651104ae7fb8a8f5b2ab4a763235
Robert Lackey <rlackey4@gmail.com>
2023-03-29 06:47:17 -0700

    updated date logic to pull last weeks of data

--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models import db, ESDForm
 from forms import SubmitForm
 from datetime import datetime, date, timedelta
-from flask_paginate import Pagination, get_page_args
 
 bp = Blueprint('routes', __name__)
 
@@ -47,23 +46,30 @@ def submit():
 
 @bp.route('/items/two-weeks-sunday')
 def show_items_last_two_weeks_sunday():
-    # Calculate start and end dates for the previous 2 weeks (Sunday to Saturday)
-    today = datetime.today().date()
-    last_saturday = today - timedelta(days=today.weekday() + 2)
-    two_weeks_ago = last_saturday - timedelta(days=13)
-    start_date = two_weeks_ago
+    # Calculate the date of the most recent Sunday
+    today = date.today()
+    days_since_sunday = (today.weekday() + 1) % 7
+    last_sunday = today - timedelta(days=days_since_sunday)
+
+    # Calculate the date of the Sunday two weeks before the most recent Sunday
+    two_weeks_ago = last_sunday - timedelta(weeks=2)
+
+    # Calculate the date of the most recent Saturday
+    last_saturday = last_sunday + timedelta(days=6)
 
     # Get ESDForm objects within the 2-week period
     data = ESDForm.query.filter(ESDForm.date.between(two_weeks_ago, last_saturday)).order_by(ESDForm.date.asc()).all()
 
     # Create a list of start and end dates for each week
     week_dates = []
+    start_date = two_weeks_ago
     while start_date <= last_saturday:
         end_date = start_date + timedelta(days=6)
         week_dates.append((start_date, end_date))
         start_date += timedelta(days=7)
 
     return render_template('items_by_week.html', data=data, week_dates=week_dates)
+
 
 @bp.route('/list')
 def list():
@@ -107,4 +113,3 @@ def delete(id):
     db.session.commit()
     flash('Form has been deleted.')
     return redirect(url_for('routes.list'))
-
